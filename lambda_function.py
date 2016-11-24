d6900f2e5038e9bba06b4cc985d688044198c4f4
vimmiswami <vimmi.swami@gmail.com>
2016-11-23 16:34:20 -0800

    small bug for size

--- lambda_function.py
+++ lambda_function.py
@@ -52,7 +52,7 @@ def get_takeone_intent_handler(request):
     if menu == "menu":
         reply = reply +"you can order: {0}".format("EXTRAVAGANZZA")
     
-    if pizza_type == None:
+    if pizza_type == None and size == None:
         reply = reply + "I could not find it, if you want me to read menu, say  provide menu"
     
     if pizza_type== "extravaganzza":
