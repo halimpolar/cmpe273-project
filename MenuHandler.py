f85bc207f0df0bc9577c074165c21bf46721c4c2
HarryKu <eternal24591563@gmail.com>
2016-12-02 20:31:55 -0800

    Modify google sheet API to work with service key

--- MenuHandler.py
+++ MenuHandler.py
@@ -1,20 +1,19 @@
 from __future__ import print_function
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+from httplib2 import Http
 from apiclient.discovery import build
-
-
-ORDER = {
-    'type': None,
-    'size': None,
-    'crusts': None,
-    'toppings': None
-}
 
 
 class MenuHandler:
     def __init__(self):
+        scopes = ['https://www.googleapis.com/auth/spreadsheets']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+                'Alexa_Pizza.json', scopes)
+        http_auth = credentials.authorize(Http())
+        self.service = build('sheets', 'v4', http=http_auth)
         self.sheetID = '1rJ6-8LPsQJFXMeQVC-RfruSuF5IQO57Oujvl2FHjgR8'
         self.sheetName = 'Menu'
-        self.service = build('sheets', 'v4', developerKey='AIzaSyCkiKIX1pHyXVZrm5lkuALr9P7cqLwDPW8')
 
     def getPizzaTypes(self):
         rangeName = self.sheetName + '!A2:A'
@@ -44,37 +43,6 @@ class MenuHandler:
         values = result.get('values', [])
         return values
 
-    # check the information we want before writting to sheet
-    def hasEnoughInfo(self):
-        global ORDER
-        for key in ORDER.keys():
-            if ORDER[key] is None:
-                return False, key
-        return True
-
-    def writeData(self, key, value):
-        global ORDER
-        ORDER[key] = value
 
 if __name__ == '__main__':
     menuHandler = MenuHandler()
-    print(menuHandler.hasEnoughInfo())
-    menuHandler.writeData('type', 'hello')
-    print(menuHandler.hasEnoughInfo())
-    toppings = ['1', '2']
-    menuHandler.writeData('toppings', toppings)
-    print(menuHandler.hasEnoughInfo())
-    menuHandler.writeData('size', 'hello2')
-    print(menuHandler.hasEnoughInfo())
-    crusts = ['1', '2']
-    menuHandler.writeData('crusts', crusts)
-    print(menuHandler.hasEnoughInfo())
-    '''
-    types = menuHandler.getPizzaType()
-    print('Types: ')
-    for pizza in types:
-        print("{}".format(pizza[0]))
-    print('Sizes: ')
-    for size in menuHandler.getPizzaSize():
-        print("{}".format(size[0]))
-    '''
